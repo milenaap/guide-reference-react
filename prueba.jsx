71adcff22e39780427066c6157ee067f38759219
Milena <darimile@gmail.com>
2025-08-07 11:25:45 +0200

    first commit

---
+++ prueba.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CounterScreen = () => {
+  return (
+    <div>CounterScreen</div>
+  )
+}
---
+++ reference-guide.js
@@ -0,0 +1,100 @@
+
+/**********************************************************
+ * 
+ * VARIABLES
+ * 
+ ***********************************************************/
+
+
+/**
+ * Hook  
+ Se importa 
+
+- useState
+
+ import { useState } from "react";
+
+ Luego en el componente se declara la variable y la funcion que la modifica
+ const [nombreVariable, setNombreVariable] = useState(valorInicial);        // Se puede inicializar con cualquier valor
+ 
+
+
+ Estructura de un JSX:
+
+import React from 'react'                   /// <------- Aquí van los import 
+const formData = {                          /// <------- NO SIEMPRE van la declaraciones de las variables y/o constantes y/o funciones
+    ...
+
+}  
+                     
+ export const reference-guide = () => {
+
+    const dispatch = useDispatch();        /// <------- Aquí van los "use..." Hooks
+
+
+    const formData = {                     /// <------- NO SIEMPRE van la declaraciones de las variables y/o constantes y/o funciones
+    ...
+
+}
+    
+    return (
+     <div>reference-guide</div>            /// <------- Aquí va el HTML y/o renderizaciones y/o llamadas a componentes
+   )
+ }
+
+
+
+
+ */
+
+ 
+ 
+
+
+ 
+ 
+
+
+
+
+/**********************************************************
+ * 
+ * VARIABLES
+ * 
+ ***********************************************************/
+
+
+/**
+ * Hook  
+ Se importa 
+
+ import { useState } from "react";
+
+ Luego en el componente se declara la variable y la funcion que la modifica
+ const [nombreVariable, setNombreVariable] = useState(valorInicial);        // Se puede inicializar con cualquier valor
+ 
+
+
+
+
+ Funcional component
+
+
+ import React from 'react'
+ 
+ export const CounterScreen = () => {
+
+    // Declaración de la variable de estado
+    const [counter, setCounter] = useState(0);
+
+    // Retorno del componente   
+   return (
+     <div>CounterScreen</div>
+   )
+ }
+
+
+
+
+ */
+
